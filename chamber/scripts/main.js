b2652cb52f4a1e8eeddb64e0eb91360caa624f53
ndamatta <damatta.natanae@gmail.com>
2023-05-12 09:41:37 -0300

    worked on w04 home page chamber of commerce

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -2,7 +2,6 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
-
 }
 
 const x = document.getElementById("hamburgerBtn")
