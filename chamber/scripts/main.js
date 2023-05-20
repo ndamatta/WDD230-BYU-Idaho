a77028fae04708c56cf45f2c4ad3dfaa3b524651
ndamatta <damatta.natanae@gmail.com>
2023-05-19 23:22:05 -0300

    worked on w05 assignment

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -9,9 +9,20 @@ x.onclick = toggleMenu;
 
 /* TODAY'S DATE IN HEADER */
 const todayDateField = document.querySelector("#todaysDate");
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+const date = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
 todayDateField.innerHTML = fulldate;
+
+/* JOIN US BANNER */
+/* Display banner if day is Monday or Wednesday, it's forcing to hide it instead */
+const meetingBanner = document.querySelector(".home-meeting-banner")
+
+if (date.getDay() === 1 || date.getDay() === 3) {
+    meetingBanner.style.display = "block";
+}
+else {
+    meetingBanner.style.display = "none";
+}
 
 /* FOOTER COPYRIGHT YEAR AND LAST MODIFIED */
 let lastModified = document.querySelector(".lastmodified")
