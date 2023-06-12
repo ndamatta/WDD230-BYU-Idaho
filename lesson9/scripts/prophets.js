d8f8046986f8175380f6e4c0e91e26510ef794e6
ndamatta <damatta.natanael@gmail.com>
2023-06-12 08:32:00 -0300

    working on displayProphets function

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -5,3 +5,23 @@ async function getProphetData() {
     const data = await response.json()
     console.table(data.prophets)
 }
+getProphetData();
+
+const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards');
+
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        h2.textContent = `${prophet.name} ________________`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ________________`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+
+    })
+}
