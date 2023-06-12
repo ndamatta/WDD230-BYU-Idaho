9106cb6691d7cb7f784bec215da86235ffd1114f
ndamatta <damatta.natanael@gmail.com>
2023-06-12 08:20:52 -0300

    created async function

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -1,1 +1,7 @@
 const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json"
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json()
+    console.table(data.prophets)
+}
